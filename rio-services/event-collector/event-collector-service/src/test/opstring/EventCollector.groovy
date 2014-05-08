9b6670bf54f2eb8df62dadac15015a5e03d76224
Rafał Krupiński <rafal.krupinski@sorcersoft.com>
2014-05-08 17:59:01 +0200

    change version to 5.0-M4-S7; downgrade versions in documentation

--- rio-services/event-collector/event-collector-service/src/test/opstring/EventCollector.groovy
+++ rio-services/event-collector/event-collector-service/src/test/opstring/EventCollector.groovy
@@ -22,8 +22,8 @@ String getConfigEntry() {
 deployment(name:'Event Collector') {
     groups System.getProperty(Constants.GROUPS_PROPERTY_NAME, System.getProperty('user.name'))
 
-    artifact id: 'service-dl',   'org.rioproject.event-collector:event-collector-proxy:5.0-M4-S6'
-    artifact id: 'service-impl', 'org.rioproject.event-collector:event-collector-service:5.0-M4-S6'
+    artifact id: 'service-dl',   'org.rioproject.event-collector:event-collector-proxy:5.0-M4-S7'
+    artifact id: 'service-impl', 'org.rioproject.event-collector:event-collector-service:5.0-M4-S7'
 
     configuration getConfigEntry()
 
